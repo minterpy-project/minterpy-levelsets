bdbebdb739ff3c9d71a8a072685c2e9bb1a5ef29
Sachin Krishnan T V <sthekke@mpi-cbg.de>
2022-11-03 15:11:46 +0100

    Rename package, fix installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Copyright (c) 2022
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or https://gitlab.hzdr.de/interpol/minterpy-levelsets for details.
+
+from setuptools import setup
+
+setup()
